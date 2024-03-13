3692655dd2d4032cd7b52cb25660b95083bac6fc
Alexandr Andreev <alexandreev1601@gmail.com>
2024-03-13 11:48:12 +0500

    add some variables lesson1:task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello World");
+﻿//Console.WriteLine("Hello World");
+int a = 100;
+Console.WriteLine(a);
+ // Типы данных целые: int, long
+ // Вещественный: float, double
+ // Символьный: char
+ // Логический: bool
+ double d = 5.55;
+ char c = '!';
+ bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
