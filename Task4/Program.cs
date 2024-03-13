5172101f976f396f64c95cfe5b9c5692067ee968
Alexandr Andreev <alexandreev1601@gmail.com>
2024-03-13 15:47:33 +0500

    add lesson1:Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿int a = 6;
+int b = 7;
+int c = 9;
+int d = 4;
+int e = 10;
+
+int max = a;
+
+if (max < b)
+{
+    max = b;
+}
+
+if (max < c)
+{
+    max = c;
+}
+
+if (max < d)
+{
+    max = d;
+}
+
+if (max < e)
+{
+    max = e;
+}
+System.Console.WriteLine(max);
