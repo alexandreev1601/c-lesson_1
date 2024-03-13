9cdf229815098de647fa43ced8fb39a32eed8031
Alexandr Andreev <alexandreev1601@gmail.com>
2024-03-13 13:18:56 +0500

    add product int,long, double

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,3 +2,18 @@
 int i2 = 6;
 int sum = i1 + i2;
 Console.WriteLine(sum);
+
+long l1 = 6;
+long suml = l1 + i1;
+Console.WriteLine(suml);
+
+double d1 = 5.5;
+double sumd = d1 + i2;
+System.Console.WriteLine(sumd);
+
+int product1 = i1 * i2;
+long product2 = l1 * i1;
+double product3 = i1 * d1;
+System.Console.WriteLine(product1);
+System.Console.WriteLine(product2);
+System.Console.WriteLine(product3);
